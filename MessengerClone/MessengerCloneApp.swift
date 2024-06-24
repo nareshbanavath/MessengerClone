40fe49c460e0a39130672430732c991e60d8a0ba
naresh.banavath <naresh.banavath@ivycomptech.com>
2024-06-24 08:29:07 +0530

    Initial Commit

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Naresh Banavath on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
